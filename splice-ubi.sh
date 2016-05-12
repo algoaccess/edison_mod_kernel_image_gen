2b580b2a73ce5e548462c5ce740dcbebb7e2893f
Yeo Kheng Meng <yeokm1@gmail.com>
2016-05-12 14:01:16 +0800

    corrected path errors in splice

--- splice-ubi.sh
+++ splice-ubi.sh
@@ -7,16 +7,16 @@ rm -rf ubilinux-150309-usb-mod
 
 tar xvf ubilinux-edison-150309.tar.gz
 mv toFlash ubilinux-150309-usb-mod
-cd ubilinux-150309-usb-mod
 
 #Replace existing Ubi kernel with new kernel
-cp edison-src/out/current/build/toFlash/edison-image-edison.hddimg .
+cp edison-src/out/current/build/toFlash/edison-image-edison.hddimg ubilinux-150309-usb-mod/
+cd ubilinux-150309-usb-mod
 
 #Replace kernel modules
 mkdir /mnt/ubi
 mount edison-image-edison.ext4 /mnt/ubi
 rm -rf /mnt/ubi/lib/modules/3.10.17-yocto-standard-r2
-cp -r edison-src/out/current/build/tmp/work/edison-poky-linux/edison-image/1.0-r0/rootfs/lib/modules/* /mnt/ubi/lib/modules/
+cp -r ../edison-src/out/current/build/tmp/work/edison-poky-linux/edison-image/1.0-r0/rootfs/lib/modules/* /mnt/ubi/lib/modules/
 umount /mnt/ubi
 cd ..
 
