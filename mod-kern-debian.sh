048b20673f9fbee9fc4507d61cc7050d7bf9bb6c
Yeo Kheng Meng <yeokm1@gmail.com>
2016-05-15 21:15:44 +0800

    ability to select sid or jessie

--- mod-kern-debian.sh
+++ mod-kern-debian.sh
@@ -39,11 +39,33 @@ make
 #Adjust bug in build path "build_dir=$top_repo_dir/build" >>> "build_dir=$top_repo_dir/out/linux64/build"
 sed -i 's/^build_dir=$top_repo_dir\/build/build_dir=$top_repo_dir\/out\/linux64\/build/' meta-intel-edison/utils/create-debian-image.sh
 
-#Change from jessie to a newer sid to use latest packages. Comment out the following line if to want use the unstable distribution sid
-if [ "$1" == "sid" ]; then
-    echo "We will use the unstable sid branch instead of the stable jessie"
-    sed -i '/^  debootstrap --arch i386 --no-check-gpg/ { s,jessie,sid, }' meta-intel-edison/utils/create-debian-image.sh
+#Change from jessie to a newer sid to use latest packages.
+if [[ $# -eq 0 ]]
+	then
+		echo "No arguments, we are staying with jessie"
+else
+    if [ "$1" == "sid" ]; then
+    	echo "We will use the unstable sid branch instead of the stable jessie"
+    	sed -i '/^  debootstrap --arch i386 --no-check-gpg/ { s,jessie,sid, }' meta-intel-edison/utils/create-debian-image.sh
+    else
+    	echo "Unknown argument, staying with jessie"
+    fi
 fi
+
+
+#!/bin/bash
+if [[ $# -eq 0 ]]
+then
+echo "No arguments, we are staying with jessie"
+else
+if [ "$1" == "sid" ]; then
+echo "We will use the unstable sid branch instead of the stable jessie"
+else
+echo "Unknown argument, staying with jessie"
+fi
+fi
+
+
 
 cd ..
 
