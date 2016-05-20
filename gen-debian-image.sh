9c4c03be90a9f4d8098701ff9b97fac035fac876
Yeo Kheng Meng <yeokm1@gmail.com>
2016-05-20 11:01:04 +0800

    mov modding of debian script to second script

--- gen-debian-image.sh
+++ gen-debian-image.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
 cd edison-src
+
+#Adjust bug in build path "build_dir=$top_repo_dir/build" >>> "build_dir=$top_repo_dir/out/linux64/build"
+sed -i 's/^build_dir=$top_repo_dir\/build/build_dir=$top_repo_dir\/out\/linux64\/build/' meta-intel-edison/utils/create-debian-image.sh
+
+#Change from jessie to a newer sid to use latest packages.
+if [ $# -eq 0 ]
+	then
+		echo "No arguments, we are staying with jessie"
+else
+    if [ "$1" == "sid" ]; then
+    	echo "We will use the unstable sid branch instead of the stable jessie"
+    	sed -i '/^  debootstrap --arch i386 --no-check-gpg/ { s,jessie,sid, }' meta-intel-edison/utils/create-debian-image.sh
+    else
+    	echo "Unknown argument, staying with jessie"
+    fi
+fi
+
 ./meta-intel-edison/utils/create-debian-image.sh
 
 cd ..
--- mod-kern-debian.sh
+++ mod-kern-debian.sh
@@ -36,22 +36,6 @@ sed -i '/^CONFIG_USB_SERIAL_DEBUG/ { s,CONFIG_USB_SERIAL_DEBUG,# CONFIG_USB_SERI
 
 make
 
-#Adjust bug in build path "build_dir=$top_repo_dir/build" >>> "build_dir=$top_repo_dir/out/linux64/build"
-sed -i 's/^build_dir=$top_repo_dir\/build/build_dir=$top_repo_dir\/out\/linux64\/build/' meta-intel-edison/utils/create-debian-image.sh
-
-#Change from jessie to a newer sid to use latest packages.
-if [ $# -eq 0 ]
-	then
-		echo "No arguments, we are staying with jessie"
-else
-    if [ "$1" == "sid" ]; then
-    	echo "We will use the unstable sid branch instead of the stable jessie"
-    	sed -i '/^  debootstrap --arch i386 --no-check-gpg/ { s,jessie,sid, }' meta-intel-edison/utils/create-debian-image.sh
-    else
-    	echo "Unknown argument, staying with jessie"
-    fi
-fi
-
 cd ..
 
 echo "To continue, you now have to run \"sudo ./gen-debian-image.sh\" to generate the Debian image."
