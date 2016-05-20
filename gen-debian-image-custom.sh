46ce6bb9e27ef9900ba659711e61bcc396af080d
Yeo Kheng Meng <yeokm1@gmail.com>
2016-05-20 12:21:55 +0800

    spelling

--- gen-debian-image-custom.sh
+++ gen-debian-image-custom.sh
@@ -24,4 +24,4 @@ rm -rf edison-image-ww25.5-15-usb-deb-mod
 mkdir edison-image-ww25.5-15-usb-deb-mod
 cp -a edison-src/out/linux64/build/toFlash/. edison-image-ww25.5-15-usb-deb-mod/
 
-echo "You can now run \"sudo ./flashall.sh\" in edison-image-ww25.5-15-usb-mod directory to flash your Intel Edison."
+echo "You can now run \"sudo ./flashall.sh\" in edison-image-ww25.5-15-usb-deb-mod directory to flash your Intel Edison."
